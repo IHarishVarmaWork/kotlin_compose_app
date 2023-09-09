60ee2d0d0ef4f2ec18f16c43d7cd42b96a5c9449
Android Developer Engineer <126979230+android-dev-engineer@users.noreply.github.com>
2023-09-09 21:05:14 +0100

    update dependencies

--- app/src/main/java/com/android/dev/engineer/kotlin/compose/app/KotlinComposeApp.kt
+++ app/src/main/java/com/android/dev/engineer/kotlin/compose/app/KotlinComposeApp.kt
@@ -1,6 +1,7 @@
 package com.android.dev.engineer.kotlin.compose.app
 
 import android.app.Application
+import com.android.dev.engineer.kotlin.compose.data.BuildConfig
 import com.android.dev.engineer.kotlin.compose.data.timber.TimberDebugTree
 import com.android.dev.engineer.kotlin.compose.ui.util.ExcludeFromJacocoGeneratedReport
 import dagger.hilt.android.HiltAndroidApp
